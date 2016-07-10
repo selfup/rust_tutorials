91cd8d156e6e19aba256a104df8fc89c069f8cd9
Regis <boudinot.regis@yahoo.com>
2016-07-10 00:21:16 -0600

    type comments

--- tut_one/src/main.rs
+++ tut_one/src/main.rs
@@ -1,27 +1,35 @@
 fn main() {
     println!("{}", first_function(0));
+    // -> ()
 }
 
 fn first_function(incoming_int: i8) -> String {
-    let int_to_str = format!("{}", incoming_int); // i8 -> &str
+    let int_to_str = format!("{}", incoming_int);
+                     // i8 => &str -> ()
 
-    if int_to_str == "0" {
+    if int_to_str == "0" { 
         return "is that your best shot?".to_string();
+                                         // &str => String -> String
     }
 
     String::from(int_to_str)
+    // &str => String -> String
 }
 
 #[test]
 fn it_can_take_an_int_and_return_a_string() {
     let expected = "12".to_string();
+                        // &str => String -> ()
 
     assert_eq!(expected, first_function(12));
+    // -> ()
 }
 
 #[test]
 fn it_returns_a_message_instead_of_a_string_representation() {
     let expected = "is that your best shot?".to_string();
+                                             // &str => String -> ()
 
-    assert_eq!(expected, first_function(0)); 
+    assert_eq!(expected, first_function(0));
+    // -> ()
 }
